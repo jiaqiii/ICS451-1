22b58da57a00772c5de66c7b51fa6786aa9a08cf
Neel Kotak <neelk@hawaii.edu>
2018-09-22 07:01:29 -0400

    Working server, io issue
    When file ends in new line, connection does not close automatically.

--- http_server.java
+++ http_server.java
@@ -1,22 +1,43 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 public class http_server{ 
     public static void main(String[] args) throws Exception { 
         // listens on port 8080
-        final ServerSocket server = new ServerSocket(8080);
+        //final ServerSocket server = new ServerSocket(8080);
         System.out.println("Listening for connection on port 8080 ...."); 
-        while (true){ // spin forever 
-            Socket client_socket = server.accept();
+        ServerSocket server = new ServerSocket(8080);
+        String text = "text.txt";
+        String line = null;
+            //Socket client_socket = server.accept();
+            try {
+                FileReader fileReader = new FileReader(text);
+                BufferedReader reader = new BufferedReader(fileReader);
+                line = reader.readLine();
+                while((line != null)) {
+                    try (Socket socket = server.accept()) {
+                        socket.getOutputStream().write(line.getBytes("UTF-8"));
+                        line = reader.readLine();
+                    }
+                }
+                if(line == null) {
+                    System.out.println("Connection Closed\n");
+                }
+                reader.close();
+            }
+
+            catch(FileNotFoundException e) {
+                System.out.println("File Not Found");
+            }
+/*
             InputStreamReader isr = new InputStreamReader(client_socket.getInputStream());
             BufferedReader reader = new BufferedReader(isr);
-            String line = reader.readLine();
+            line = reader.readLine();
             while(!line.isEmpty()) {
-                System.out.println(line);
+                socket.getOutputStream().write(line);
                 line = reader.readLine();
             }
+*/
             /*
             1. Read HTTP request from the client socket
             2. Prepare an HTTP response
@@ -24,6 +45,5 @@ public class http_server{
             4. Close the socket
             */
         } 
-    } 
 }
 
