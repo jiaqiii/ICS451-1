0123418d3ef2de07debc7db143c045845a43f74e
Neel Kotak <neelk@hawaii.edu>
2018-09-22 18:27:12 -0400

    Working server, need to work on client now

--- http_server.java
+++ http_server.java
@@ -1,49 +1,26 @@
 import java.io.*;
+import java.util.Scanner;
 import java.net.ServerSocket;
 import java.net.Socket;
 public class http_server{ 
     public static void main(String[] args) throws Exception { 
-        // listens on port 8080
-        //final ServerSocket server = new ServerSocket(8080);
-        System.out.println("Listening for connection on port 8080 ...."); 
-        ServerSocket server = new ServerSocket(8080);
-        String text = "text.txt";
-        String line = null;
-            //Socket client_socket = server.accept();
-            try {
-                FileReader fileReader = new FileReader(text);
-                BufferedReader reader = new BufferedReader(fileReader);
-                line = reader.readLine();
-                while((line != null)) {
-                    try (Socket socket = server.accept()) {
-                        socket.getOutputStream().write(line.getBytes("UTF-8"));
-                        line = reader.readLine();
-                    }
-                }
-                if(line == null) {
-                    System.out.println("Connection Closed\n");
-                }
-                reader.close();
+        int port = Integer.parseInt(args[0]);
+        String sending_msg = "Receiving message from server\n\n";
+        System.out.println("Listening for connection on port "+port+"..."); 
+        // Reading text file into scanner to be put into a String
+        Scanner scanner = new Scanner(new File("ArduinoIDE_guide_galileo.html"), "UTF-8");
+        String text = scanner.useDelimiter("\\A").next();
+        scanner.close();
+        // open up port for listening
+        ServerSocket server = new ServerSocket(port);
+        // Port is kept open 
+        while(true) {
+            try (Socket socket = server.accept()) {
+                // Sending file to connected client
+                socket.getOutputStream().write(sending_msg.getBytes("UTF-8"));
+                socket.getOutputStream().write(text.getBytes("UTF-8"));
             }
-
-            catch(FileNotFoundException e) {
-                System.out.println("File Not Found");
-            }
-/*
-            InputStreamReader isr = new InputStreamReader(client_socket.getInputStream());
-            BufferedReader reader = new BufferedReader(isr);
-            line = reader.readLine();
-            while(!line.isEmpty()) {
-                socket.getOutputStream().write(line);
-                line = reader.readLine();
-            }
-*/
-            /*
-            1. Read HTTP request from the client socket
-            2. Prepare an HTTP response
-            3. Send HTTP response to the client
-            4. Close the socket
-            */
-        } 
+        }
+    }
 }
 
