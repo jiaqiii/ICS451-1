ad22be8e1396ad77e1686ef1d4f678ffe0d0d9f7
Neel Kotak <neelk@hawaii.edu>
2018-09-22 19:42:05 -0400

    Refined server messages and comments

--- http_server.java
+++ http_server.java
@@ -5,21 +5,33 @@ import java.net.Socket;
 public class http_server{ 
     public static void main(String[] args) throws Exception { 
         int port = Integer.parseInt(args[0]);
-        String sending_msg = "Receiving message from server\n\n";
+
+        // Lets client know message is coming and when it is fully delivered
+        // Will be printed onto client screen
+        String sending_msg = "Receiving message from server.\n\n";
+        String sent_msg = "Message was fully received above.\n";
+
+        // open up port for listening
+        ServerSocket server = new ServerSocket(port);
         System.out.println("Listening for connection on port "+port+"..."); 
+
         // Reading text file into scanner to be put into a String
         Scanner scanner = new Scanner(new File("ArduinoIDE_guide_galileo.html"), "UTF-8");
         String text = scanner.useDelimiter("\\A").next();
         scanner.close();
-        // open up port for listening
-        ServerSocket server = new ServerSocket(port);
-        // Port is kept open 
+
+        // Port is kept open for connection
         while(true) {
-            try (Socket socket = server.accept()) {
-                // Sending file to connected client
-                socket.getOutputStream().write(sending_msg.getBytes("UTF-8"));
-                socket.getOutputStream().write(text.getBytes("UTF-8"));
-            }
+            // Created socket to accept connections from client
+            Socket socket = server.accept(); 
+
+            // Sending file to connected client
+            socket.getOutputStream().write(sending_msg.getBytes("UTF-8"));
+            socket.getOutputStream().write(text.getBytes("UTF-8"));
+            socket.getOutputStream().write(sent_msg.getBytes("UTF-8"));
+
+            // Close socket and connection
+            socket.close();
         }
     }
 }
