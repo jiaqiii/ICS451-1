d3066ed621d93ffa050578d9af2721fa67239389
Neel Kotak <neelk@uhx02.its.hawaii.edu>
2018-09-24 18:17:19 -1000

    completed basic functionality of hw

--- client.java
+++ client.java
@@ -1,5 +1,22 @@
+import java.io.*;
+import java.net.*;
 public class client {
     public static void main(String[] args) throws IOException {
-        Socket 
+        Socket client_socket;
+        BufferedReader in;
+
+        int port = Integer.parseInt(args[0]);
+        client_socket = new Socket("localhost", port);
+        in = new BufferedReader(new InputStreamReader(client_socket.getInputStream()));
+        StringBuffer string_buffer = new StringBuffer();
+        String line;
+        while ((line = in.readLine()) != null) {
+            string_buffer.append(line);
+            string_buffer.append("\n");
+        }
+        System.out.println(string_buffer.toString());
+        in.close();
+        System.out.println("Closing Connection to server");
+        client_socket.close();
     }
 }
